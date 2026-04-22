6fc264486baac46b46e19b299706b5e845241d31
Bedram Tamang <tmgbedu@gmail.com>
2026-04-22 14:23:51 -0700

    feat: database providers

--- example/inertia-app/bootstrap/application.py
+++ example/inertia-app/bootstrap/application.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 
-from fastapi_startkit.application import Application
-from fastapi_startkit.logging.providers import LogProvider
+from fastapi_startkit import Application
+from fastapi_startkit.logging import LogProvider
 from fastapi_startkit.vite import ViteProvider
 from fastapi_startkit.inertia import InertiaProvider
-from fastapi_startkit.masoniteorm.providers import DatabaseProvider
+from fastapi_startkit.masoniteorm import DatabaseProvider
 
 from providers.fastapi_provider import FastAPIProvider
 
