180e6964f57cb3bf720bbbe7efde8a7b7ed423f7
Bedram Tamang <tmgbedu@gmail.com>
2026-05-18 01:45:14 -0700

    feat: dx fixes

--- fastapi_startkit/src/fastapi_startkit/storage/__init__.py
+++ fastapi_startkit/src/fastapi_startkit/storage/__init__.py
@@ -1,3 +1,6 @@
 from .storage import Storage
 from .config import S3Config, LocalDiskConfig, PublicDiskConfig
 from .drivers.fake import FakeDriver
+from .providers.provider import StorageProvider
+
+__all__ = ["Storage", "S3Config", "LocalDiskConfig", "PublicDiskConfig", "FakeDriver", "StorageProvider"]
